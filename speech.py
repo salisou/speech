cf9f11b1949e39742ca686b74b2a768f2024cfee
Moussa <61307355+salisou@users.noreply.github.com>
2021-10-02 11:32:50 +0200

    Update speech.py

--- speech.py
+++ speech.py
@@ -5,7 +5,7 @@ with sr.Microphone() as source:
     print("Speak Anything :")
     audio = r.listen(source)
     try:
-        text = r.recognize_google(audio)
+        text = r.recognize_google(audio) # pprint(r.recognize_google(audio, show_all=True))
         print("You said : {}".format(text))
     except:
         print("Sorry could not recognize what you said")
